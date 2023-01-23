08d26f5346bb6671b32d04a55c6025197610b76e
Loris019 <lorisbrignatz@free.fr>
2023-01-23 16:48:57 +0100

    Ajout des Tests unitaires pour les fonctions de la classe LigneService

--- src/test/java/comptoirs/service/LigneServiceTest.java
+++ src/test/java/comptoirs/service/LigneServiceTest.java
@@ -8,6 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import jakarta.validation.ConstraintViolationException;
+import java.util.Optional;
+import java.util.NoSuchElementException;
+
+import comptoirs.entity.Commande;
+import comptoirs.entity.Ligne;
+import comptoirs.entity.Produit;
+import comptoirs.dao.ProduitRepository;
+import comptoirs.dao.CommandeRepository;
+
 
 @SpringBootTest
  // Ce test est basé sur le jeu de données dans "test_data.sql"
@@ -20,9 +29,17 @@ class LigneServiceTest {
     static final int REFERENCE_PRODUIT_DISPONIBLE_4 = 96;
     static final int REFERENCE_PRODUIT_INDISPONIBLE = 97;
     static final int UNITES_COMMANDEES_AVANT = 0;
+    static final int REFERENCE_PRODUIT_INEXISTANTE = 100;
+    static final int NUMERO_COMMANDE_INEXISTANTE = 91;
 
     @Autowired
     LigneService service;
+
+    @Autowired
+    ProduitRepository produitDao;
+
+    @Autowired
+    CommandeRepository commandeDao;
 
     @Test
     void onPeutAjouterDesLignesSiPasLivre() {
@@ -36,5 +53,35 @@ class LigneServiceTest {
         assertThrows(ConstraintViolationException.class, 
             () -> service.ajouterLigne(NUMERO_COMMANDE_PAS_LIVREE, REFERENCE_PRODUIT_DISPONIBLE_1, 0),
             "La quantite d'une ligne doit être positive");
+    }
+
+    @Test
+    void testAjoutLigneDejaEnvoyée() {
+        Integer commandeNum = NUMERO_COMMANDE_DEJA_LIVREE;
+        Integer produitRef = REFERENCE_PRODUIT_DISPONIBLE_1;
+        int quantite = 1;
+        assertThrows(IllegalArgumentException.class, () -> service.ajouterLigne(commandeNum, produitRef, quantite));
+    }
+
+    @Test
+    void testAjoutProdInconnu() {
+        Integer commandeNum = NUMERO_COMMANDE_PAS_LIVREE;
+        Integer produitRef = REFERENCE_PRODUIT_INEXISTANTE;
+        int quantite = 1;
+
+        assertEquals(Optional.empty(), produitDao.findById(produitRef));
+        assertNotNull(commandeDao.findById(commandeNum));
+        assertThrows(NoSuchElementException.class, () -> service.ajouterLigne(commandeNum, produitRef, quantite));
+    }
+
+    @Test
+    void testAjoutCommInconnue() {
+        Integer commandeNum = NUMERO_COMMANDE_INEXISTANTE;
+        Integer produitRef = REFERENCE_PRODUIT_DISPONIBLE_1;
+        int quantite = 1;
+
+        assertEquals(Optional.empty(), commandeDao.findById(commandeNum));
+        assertNotNull(produitDao.findById(produitRef));
+        assertThrows(NoSuchElementException.class, () -> service.ajouterLigne(commandeNum, produitRef, quantite));
     }
 }
